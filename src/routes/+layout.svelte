aa07f52be32e87e4355f385147b86c55f799201f
Svralabs <svralabs@gmail.com>
2026-08-09 01:54:43 +0700

    feat: update Svralabs branding, custom case study pages, logo assets and favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { pageTransition } from '$lib/actions/motion';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" type="image/png" href="/logowotext.png" />
+	<link rel="apple-touch-icon" href="/logowotext.png" />
+</svelte:head>
+
 <div use:pageTransition class="w-full min-h-screen">
 	{@render children()}
 </div>
